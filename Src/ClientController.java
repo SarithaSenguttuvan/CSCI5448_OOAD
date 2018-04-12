dece3d2e0bcda05e6ce934ab0705e1cb42571024
saritha <sase6967@colorado.edu>
2018-04-12 05:34:30 +0000

    Chaned Client Controller

--- Src/ClientController.java
+++ Src/ClientController.java
@@ -2,6 +2,7 @@ public class ClientController
 {
     public static void main(String[] args)
     {
+        //display logo
 		System.out.println("Hello! Welcome to the Smart Home page");
 		System.out.println("1. Admin login");
 		System.out.println("2. Customer login");
