2c5bdb6e425a9d7e0f3e65f267396af7244da147
rajkumarsubramaniam <raj.subramaniam@colorado.edu>
2018-04-11 23:19:43 -0600

    Sensor class updated
    Added code to sensor class

--- Src/Sensor.java
+++ Src/Sensor.java
@@ -4,7 +4,16 @@ public class Sensor extends Product
     private String location;
     private double threashold;
     private String helpInfo;
+    private boolean power;
     
+    public boolean getPower()
+    {
+        return power;
+    }
+    public void setPower(boolean _power)
+    {
+        power = _power;
+    }
     public double getValue() 
     {
         return value;
@@ -31,26 +40,39 @@ public class Sensor extends Product
     }
     public void displayHelpInfo()
     {
-        
+        System.out.println(helpInfo);
     }
     public boolean viewSettings()
     {
+        System.out.println("Location: "+ getLocation());
+        System.out.println("Threshold: "+ getThreshold());
+        System.out.println("Value: "+ getValue());
+        System.out.println("Power Status: "+ getPower());
         return true;
     }
     public void changeLocation(String _loc)
     {
-        
+        setLocation(_loc);
     }
     public void changeThreshold(double _threshold)
     {
-        
+        setThreshold(_th);
     }
     public boolean checkRange()
     {
+        if(getValue() > getThreshold())
+        {
+            notifyUser("Sensor went over the threshold value" + getValue()
+        }
+        else
+        {
+            
+        }
         return true;
     }
     public void notifyUser(String _data)
     {
-        
+        System.out.println(_data);
     }
+    
 }
