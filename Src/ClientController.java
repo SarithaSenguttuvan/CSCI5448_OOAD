732328c665ca885c209bece9d3e9ce4a16048b5e
saritha <twinsarita@gmail.com>
2018-04-12 05:07:38 +0000

    Added functionalities

--- Src/ClientController.java
+++ Src/ClientController.java
@@ -2,6 +2,10 @@ public class ClientController
 {
     public static void main(String[] args)
     {
-        
+		System.out.println("Hello! Welcome to the Smart Home page");
+		System.out.println("1. Admin login");
+		System.out.println("2. Customer login");
+		int userEntry = System.console().readLine();
+        return;        
     }
 }
