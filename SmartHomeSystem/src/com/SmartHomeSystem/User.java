03a4996cd325e7bbcc3eec8318727baf001a70de
Saritha Senguttuvan <twinsarita@gmail.com>
2018-04-27 23:10:21 +0000

    Added Factory Patter

--- SmartHomeSystem/src/com/SmartHomeSystem/User.java
+++ SmartHomeSystem/src/com/SmartHomeSystem/User.java
@@ -7,9 +7,4 @@ import javax.persistence.*;
 public class User extends Person //implements DisplayList
 {
 
-//    User(String _name, String _password, String phoneNumber)
-//    {
-//    	super(_name, _password, phoneNumber);
-//    	System.out.println("Admin constructor called");
-//    }
 }
