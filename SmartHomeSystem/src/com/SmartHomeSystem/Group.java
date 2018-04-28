178a359ca084370c175c01e2f6a5377e62615af7
savitha <twinsavita@gmail.com>
2018-04-28 23:10:52 +0000

    Group Sensors adding-Not tested

--- SmartHomeSystem/src/com/SmartHomeSystem/Group.java
+++ SmartHomeSystem/src/com/SmartHomeSystem/Group.java
@@ -1,7 +1,13 @@
 package com.SmartHomeSystem;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.persistence.*;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 @Entity
 @Table(name="GroupTable")
@@ -17,9 +23,19 @@ public class Group implements DisplayList
 	@Column(name = "Power")
 	private boolean grpSpecificPower;
 	
-	@Column(name = "SensorList")
-	public ArrayList<Sensor> gsensorList = new ArrayList<Sensor>();
+	@OneToMany(fetch = FetchType.EAGER)
+	private List<Sensor> gsensorList = new ArrayList<Sensor>();
+	
+	public List<Sensor> getgsensorList()
+	{
+		return this.gsensorList;
+	}
     
+	public void setgSensorList(Sensor s)
+	{
+		this.gsensorList.add(s);
+	}
+	
     public void setGrpName(String _grpName)
 	{
 	    grpName = _grpName;
@@ -40,7 +56,34 @@ public class Group implements DisplayList
 
     public void addSensorToGrp(Sensor _sensor)
     {
-        gsensorList.add(_sensor);
+       // gsensorList.add(_sensor);
+    	int selectedSensor;
+    	ClientController.getNetwork().listSensors();
+		selectedSensor = DisplayView.requestSensorOption();
+		DisplayView.displayInfo("Sensor selected successfully for the Group");
+    	List querySensors;
+        Sensor sensor = null;
+        Session session = ClientController.getSessionFactory().openSession();
+        Transaction tx = null;
+        try 
+        {
+            tx = session.beginTransaction();
+            sensor = ((Sensor)session.get(Sensor.class, selectedSensor));
+            this.setgSensorList(sensor);
+            session.update(this);
+            tx.commit();
+        }
+        catch(HibernateException e)
+        {
+            DisplayView.displayInfo("Sensor retrival exception");
+            if(tx!=null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+        finally
+        {
+            session.close();
+        }
     }
     
     public void removeSensorFromGrp(Sensor _sensor)
@@ -62,8 +105,10 @@ public class Group implements DisplayList
     }
     public void listSensors()
     {
-        for (Sensor _gsensorList : gsensorList) {
-            System.out.println("_gsensorList = " + _gsensorList);
+    	System.out.println("************* Group Sensors *********************");
+	    for (Sensor _sensor : this.getgsensorList()) 
+	    {
+            System.out.println(_sensor.getId()+". " +_sensor.getName());
         }
     }
 }
